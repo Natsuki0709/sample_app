4240be0af2511aed60f55543acefc53ad7baf2d7
Natsuki0709 <141482757+Natsuki0709@users.noreply.github.com>
2023-08-26 05:19:30 +0000

    [finish]3-2

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,13 +1,17 @@
 class ListsController < ApplicationController
   def new
+    @list =List.new
   end
 
-  def index
-  end
+def create
+   list = List.new(list_params)
+    list.save
+    redirect_to '/top'
+ end
 
-  def show
-  end
+private
 
-  def edit
+  def list_params
+    params.require(:list).permit(:title, :body)
   end
 end
